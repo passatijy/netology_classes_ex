73233f3e601af77fd8e3ed438d00b1a2653295e7
passatijy <lmar@mail.ru>
2019-06-27 22:49:54 +0300

    re-designed

--- animals.py
+++ animals.py
@@ -1,6 +1,5 @@
 '''
 Вы приехали помогать на ферму Дядюшки Джо и видите вокруг себя множество разных животных:
-
 attributes:
 	types
 		voice
@@ -16,7 +15,6 @@ interfaces:
 		cut
 	chicken, duck, goose
 		egg
-
 
 гусей "Серый" и "Белый"
 корову "Маньку"
@@ -59,32 +57,80 @@ list_animals = [
 
 
 class Animals:
-	def __init__(self, name, voice, hungry, legs = 4, weight = 1 ):
+	''' Main class for all animals.'''
+	def __init__(self,anim_type, name, voice, hungry, legs = 4, weight = 1 ):
+		self.anim_type = anim_type
 		self.name = name
 		self.voice = voice
 		self.hungry = hungry
+		self.legs = legs
+		self.weight = weight
 		self.classname = ''
 
 	def feed(self):
 		self.hungry = False
+		return self.hungry
 
+class Animals_with_resource(Animals):
+	def __init__(self,resource_name,resource_action_name,quantity):
+		self.resource_name = resourse_name
+		self.resource_action_name = resource_action_name
+		self.quantity = quantity
+	def get_resource(self, value = 1):
+		if value < self.quantity :
+			self.quantity = self.quantity - value
+		return [self.resource_name, resource_action_name, value]
 
-class Milking(Animals):
+class Cows(Animals_with_resource):
 	def __init__(self):
-	self.milk = 5
-	self.resourse_name = 'молоко'
-	def get_milk(self, value = 1):
-		if value < self.milk :
-			self.milk = self.milk - value
-		return [self.milk, value]
-
-class Cows(Milking):
-	self.classname = 'cow'
-	self.voice = 'муууу'
+		self.voice = 'мууу'
+		Animals_with_resource.__init__(self)
+	def voice(self):
+		return self.voice
 	
-class Goats(Milking):
-	self.classname = 'goat'
-	self.voice = 'беее'
+	
+class Goats(Animals_with_resource):
+	def __init__(self):
+		self.voice = 'беее'
+		Animals_with_resource.__init__(self)
+	def voice(self):
+		return self.voice
+	
+
+
+
+
+# Создаю список со всеми животными:
+pet_obj = []
+i = 0
+for one_pet in list_animals:
+	i = i + 1
+	pet_obj.append(Animals(one_pet['anym_type'], one_pet['anym_name'],'',True))
+#	print(one_pet)
+#	print('Type: ',one_pet['anym_type'], 'Name: ',one_pet['anym_name'])
+'''
+	if one_pet['anym_type'] == 'goat':
+		pet_obj.append(Animals('goat', one_pet['anym_name'],'',True))
+	elif one_pet['anym_type'] == 'hen':
+		pet_obj.append(Animals('hen', one_pet['anym_name'],'',True))
+	else: 
+'''
+	
+	#print(pet_obj[i].name,' ',pet_obj[i].weight)
+
+# Вывожу список со всеми животными:
+for other_pet in pet_obj:
+	#принт для отладки:
+	#print(type(other_pet))
+	print('Тип: ', other_pet.anim_type,' Имя: ', other_pet.name,' Вес: ',other_pet.weight, ' Голодное: ', other_pet.hungry, ' Голос: ',other_pet.voice)
+
+print("Животное по имени ", pet_obj[0].name," голодное?", pet_obj[0].hungry)
+pet_obj[0].feed
+print("Животное по имени ", pet_obj[0].name," голодное?", pet_obj[0].hungry)
+
+
+
+'''
 
 	
 class Sheep(Animals):
@@ -110,9 +156,7 @@ class Ducks(Birds):
 class Gooses(Birds):
 	classname = 'goose'
 	pass
+'''
 
 
-anim1 = Cows('Милка','мууу',True)
-
-print('Животное 1: ', anim1.classname, '  Оно может давать ', anim1.resource_name, 'было ', anim1.get_milk(3)[0], ' осталось ', anim1.get_milk(5)[1])
 
